1953986a54f61ce0cad662543f8398e8f0453fee
huang yi <huangered@hotmail.com>
2016-01-12 07:21:22 -0500

    123

--- server/src/parser/parser.go
+++ server/src/parser/parser.go
@@ -21,11 +21,43 @@ func (p *Parser) Parse(path string, obj interface{}) interface{} {
 
   reader := bufio.NewReader(file)
   scanner := bufio.NewScanner(reader)
-    map := make(map[string]string)
+    kvp := make(map[string]string)
   for scanner.Scan() {
     line := scanner.Text())
     objs = strings.split(line," ")
-    map[objs[0]]= objs[1]
+    kvp[objs[0]]= objs[1]
   }
+  
+  
+s := reflect.ValueOf(&obj).Elem()
+ if s.Kind() == reflect.Struct {
+   for k, v := range kvp {
+      // exported field
+        f := s.FieldByName(k)
+        if f.IsValid() {
+            // A Value can be changed only if it is 
+            // addressable and was not obtained by 
+            // the use of unexported struct fields.
+            if f.CanSet() {
+                // change value of N
+                if f.Kind() == reflect.Int {
+                    x := int64(v)
+                    if !f.OverflowInt(x) {
+                        f.SetInt(x)
+                    }
+                }else if f.Kind() == reflect.Float64 {
+                    x := float64(v)
+                    if !f.OverflowFloat(x) {
+                        f.SetFloat64(x)
+                    }
+                } else if f.Kind() ==reflect.String {
+                  f.SetString(v)
+                }
+            }
+        }
+    }
+       
+    }
+  
   return obj
 }
