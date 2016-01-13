00351c9b32c5d5f7ddb7f50205284d26f8771968
huang yi <huangered@hotmail.com>
2016-01-13 02:12:25 -0500

    Update config.go

--- server/src/parser/config.go
+++ server/src/parser/config.go
@@ -1,7 +1,19 @@
 package parser
 
 type Config struct {
-  IndexRoot string `json:"indexRoot"`
+  volumeIndex string `json:"volume_index"`
+freeVolumeIndex string `json:"free_volume_index"`
+volumeCacheSize int `json:"volume_cache_size"`
+serverId string `json:"server_id"`
+index string `json:"index"`
+needleMaxSize int `json:"needle_max_size"`
+batchMaxNum int `json:"batch_max_num"`
+
+sync int `json:"sync"`
+
+listen string `json:"listen"`
+addrs string `json:"addrs"`
+}
 }
 
 func NewConfig() *Config {
